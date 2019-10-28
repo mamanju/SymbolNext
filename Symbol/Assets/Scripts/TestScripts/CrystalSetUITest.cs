5fde8f3f2dcb72233e080e1b1bf64abf3a071dfe
mamanju <himazin19970630@gmail.com>
2019-10-28 14:04:33 +0900

    コントローラーからキーボードマウスへ移行

---
+++ Symbol/Assets/Scripts/TestScripts/CrystalSetUITest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSetUITest : MonoBehaviour
+{
+    private Vector3 worldMousePos;
+    private int mouseState = 0;
+    private GameObject clickedGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    GameObject hitObj = null;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit = new RaycastHit();
+        //    if(Physics.Raycast(ray, out hit))
+        //    {
+        //        hitObj = hit.collider.gameObject;
+        //    }
+        //    Debug.Log("Object=" + hitObj);
+        //}
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+
+
+
+    }
+
+    void CatchCrystal()
+    {
+        
+    }
+
+    void DivideCrystal()
+    {
+
+    }
+
+    void ClickCheck(GameObject obj)
+    {
+        if (mouseState == 0)
+        {
+            CatchCrystal();
+        }
+        else
+        {
+            DivideCrystal();
+        }
+    }
+}
---
+++ Symbol/Assets/Scripts/TestScripts/SynthesisTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct CrystalTest
+{
+    public int ID;
+}
+
+public class SynthesisTest : MonoBehaviour
+{
+    // 合成箱(012,345,678)
+    private CrystalTest[] synthesisBox = new CrystalTest[9];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
